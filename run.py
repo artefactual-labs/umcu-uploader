61d26af119a608203f7e0d8419309d44e1011cbc
Diogenesoftoronto <diogenesoft@protonmail.com>
2022-10-13 19:56:05 -0400

    remove venv and remove conflicts also add finish the form

---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+
+from uploader import create_app
+
+if __name__ == "__main__":
+    config_name = os.environ.get("FLASK_CONFIG", "default")
+    app = create_app(config_name)
+    app.run(host="0.0.0.0")
