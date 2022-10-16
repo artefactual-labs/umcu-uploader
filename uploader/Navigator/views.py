7c3c4422b10e80f0bb395e0b61a929c8f0423f04
Mike Cantelon <mcantelon@gmail.com>
2022-10-16 15:55:41 -0700

    Add ability to set permissions in file navigator.
    * Add ability to set directory/file permissions via file navigator
    * Store directory/file permission in JSON file
    * Allow navigator transfer directory to be set from configuration
    * Remove file viewing (to prevent accidental download of large files,
      etc.)
    * Show file MIME type in file navigator
    * Add logic for writing CSV file from permissions data
    * Add "preview" action to download permissions data as CSV

--- uploader/Navigator/views.py
+++ uploader/Navigator/views.py
@@ -1,27 +1,29 @@
 # -*- coding: utf-8 -*-
 
+import hashlib
 import os
-from flask import Blueprint, redirect, url_for, abort, render_template, send_file
+import tempfile 
 
-BASE_DIR = '/tmp'
+from flask import Blueprint, redirect, abort, render_template, session, send_file, request, flash, current_app
+import magic
+
+from uploader.Navigator import permissions
+from uploader.Transfer import helpers
 
 navigator = Blueprint("navigator", __name__, template_folder="templates")
 
-#@navigator.route("/files", methods=["GET"])
-@navigator.route('/', defaults={'req_path': ''})
-@navigator.route('/<path:req_path>')
+@navigator.route('/', defaults={'req_path': ''}, methods=["GET", "POST"])
+@navigator.route('/<path:req_path>', methods=["GET", "POST"])
 def index(req_path):
-    """Define handling for application's / route."""
-    # Joining the base and the requested path
-    abs_path = os.path.join(BASE_DIR, req_path)
+    transfer_dir = helpers.get_transfer_directory()
 
-    # Return 404 if path doesn't exist
-    if not os.path.exists(abs_path):
+    # Joining the transfer directory and the requested path
+    abs_path = os.path.join(transfer_dir, req_path)
+    permission_file_path = os.path.join(transfer_dir, permissions.PERMISSION_METADATA_FILENAME)
+
+    # Return 404 if path doesn't exist or path is a file
+    if not os.path.exists(abs_path) or os.path.isfile(abs_path):
         return abort(404)
-
-    # Check if path is a file and serve
-    if os.path.isfile(abs_path):
-        return send_file(abs_path)
 
     # Assemble back link path
     back_path = ""
@@ -29,16 +31,52 @@ def index(req_path):
         back_path = "/files/" + "/".join(req_path.split("/")[0:-1])
         back_path = back_path.rstrip("/")
 
-    # Show directory contents
+    # Get directory contents
     files = os.listdir(abs_path)
+
+    # Update permissions data, if sent
+    if request.method == 'POST':
+        for file in files:
+            entry_path = os.path.join(abs_path, file)
+            form_field_name = "perm_" + hashlib.md5(entry_path.encode('utf-8')).hexdigest()
+
+            if form_field_name in request.form:
+                permission = request.form[form_field_name]
+
+                permissions.set_permission(permission_file_path, entry_path, permission)
+
+        flash("Updated.", "primary")
+        redirect(request.path)
+
+    # Assemble directory data
+    permission_data = permissions.read_permissions(permission_file_path)
 
     entries = []
     for file in files:
         entry_path = os.path.join(abs_path, file)
 
-        entries.append({
+        entry = {
             "name": file,
-            "size": os.path.getsize(entry_path)
-        })
+            "size": os.path.getsize(entry_path),
+            "is_dir": os.path.isdir(entry_path),
+            "path_md5": hashlib.md5(entry_path.encode('utf-8')).hexdigest()
+        }
 
-    return render_template('files.html', entries=entries, back_path=back_path)
+        if not entry["is_dir"]:
+            entry["mimetype"] = magic.from_file(entry_path, mime = True)
+
+        if entry_path in permission_data:
+            entry["permission"] = permission_data[entry_path]
+
+        entries.append(entry)
+
+    return render_template('files.html', entries=entries, req_path=req_path, back_path=back_path, permissions_filename=permissions.PERMISSION_METADATA_FILENAME)
+
+@navigator.route('/preview', methods=["GET"])
+def preview():
+    transfer_dir = helpers.get_transfer_directory()
+    csv_tempfile = tempfile.NamedTemporaryFile()
+
+    permissions.write_permissions_to_csv(transfer_dir, csv_tempfile.name)
+
+    return send_file(csv_tempfile.name, as_attachment=True, mimetype="text/csv", download_name="metadata.csv")
