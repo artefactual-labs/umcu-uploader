d9a15fe44da7ac57d2a058f163165e4d34838be9
Mike Cantelon <mcantelon@gmail.com>
2022-10-20 09:56:17 -0700

    Fixes to transfer page.
    * Use Flask flash messages for status updates
    * Remove hardcoded URLs
    * Remove unused parameter in route definition
    * Add ability to lock transfer location using an env var
    * Show number of files in transfer and total file size

--- uploader/Transfer/views.py
+++ uploader/Transfer/views.py
@@ -1,29 +1,37 @@
 # -*- coding: utf-8 -*-
 
 import os
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, render_template, request, session, flash, redirect, url_for, current_app
+from flask_api import status
+
+from uploader.Transfer import helpers
 
 transfer = Blueprint("transfer", __name__, template_folder="templates")
 
-@transfer.route('/', defaults={'req_path': ''})
-@transfer.route('/<path:req_path>')
-def index(req_path):
-    transfer_dir = ""
-    if "transfer_directory" in session:
-        transfer_dir = session["transfer_directory"]
+@transfer.route('/')
+def index():
+    transfer_dir = helpers.get_transfer_directory()
+    transfer_preset = helpers.transfer_directory_set_in_config()
+    transfer_file_count, transfer_total_file_size = helpers.directory_count_and_size(transfer_dir)
 
-    return render_template('transfer.html', transfer_directory=transfer_dir)
+    return render_template('transfer.html', transfer_directory=transfer_dir, transfer_preset=transfer_preset, transfer_file_count=transfer_file_count, transfer_total_file_size=transfer_total_file_size)
 
 @transfer.route('/transfer', methods=["POST"])
 def update():
-    if "transfer_directory" in request.form:
-        transfer_dir = request.form["transfer_directory"]
+    transfer_dir = request.form["transfer_directory"]
+
+    if transfer_dir:
         if not os.path.exists(transfer_dir):
-            message = "Path does not exist."
+            flash("Path does not exist.", "danger")
+            return redirect(url_for('transfer.index'))
         elif os.path.isfile(transfer_dir):
-            message = "Path is a file not a directory."
+            flash("Path is a file not a directory.", "danger")
+            return redirect(url_for('transfer.index'))
         else:
             session["transfer_directory"] = request.form["transfer_directory"]
-            message = "Updated."
+            session.modified = True
 
-    return render_template('transfer.html', message=message)
+            flash("Updated.", "primary")
+            return redirect(url_for('transfer.index'))
+
+    return "Unsuitable form data submitted", status.HTTP_400_BAD_REQUEST
