7d2416d5e87ceebc1f8159d09ee8ba951521bd06
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-10-27 15:18:49 -0400

    add form validation class

---
+++ uploader/Metadata/form_class.py
@@ -0,0 +1,20 @@
+from wtforms import Form, TextAreaField, DateField, SelectField, StringField, validators, ValidationError
+
+class MetadataForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=255)])
+    division = SelectField('umcu-division', [validators.Length(min=1, max=255)])
+    description = TextAreaField('Description', [validators.Length(min=1)])
+    author = StringField('author', [validators.Length(min=1)])
+    contributor = StringField('Contributor')
+    deposit_date = DateField('deposit-date', [validators.Length(min=1, max=255)])
+    research_end_date = DateField('research-end-date', [validators.Length(min=1, max=255)])
+    date_range_start = DateField('date-range-start', [validators.Length(min=1, max=255)])
+    date_range_end = DateField('date-range-end', [validators.Length(min=1, max=255)])
+    data_type = StringField('data-type', [validators.Length(min=1, max=255)])
+    licence = SelectField('licence-type', [validators.Length(min=1, max=255)])
+    research_type = StringField('research-type', [validators.Length(min=1, max=255)])
+    software = StringField('software')
+    language = StringField('language')
+    related_publication = StringField('related-publication', [validators.Length(min=1, max=255)])
+    keyword = StringField('keyword', [validators.Length(min=1, max=255)])
+    package_type = SelectField('package-type', [validators.Length(min=1, max=255)])
