ad0a8869a65c9115b24adf4274f38b901ea00dfb
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-11-15 17:00:28 -0500

    more changes to api dv request and json

--- uploader/Metadata/views.py
+++ uploader/Metadata/views.py
@@ -2,6 +2,7 @@
 from flask import Blueprint, render_template, request, jsonify, redirect
 from json import dump, dumps
 import requests
+from string import Template
 from os import getenv
 
 api_token = getenv('API_TOKEN')
@@ -24,28 +25,28 @@ def index(req_path):
         author_values = [{
             "authorName": {
                 "typeName": "authorName",
-                "multiple": 'false',
+                "multiple": False,
                 "typeClass": "primitive",
                 "value": x
             }} for x in author_values_temp]
-        subject_value = 'Medicine, Health and Life Sciences'
+        subject_value = ['Medicine, Health and Life Sciences']
         keyword_values_temp = [
             y for x, y in form_data.items() if x.startswith('keyword')]
         keyword_values = [{'keywordValue': {
             'typeName': 'keywordValue',
-            'multiple': 'false',
+            'multiple': False,
             'typeClass': 'primitive',
             'value': x}} for x in keyword_values_temp]
         publication_values_temp = [
             y for x, y in form_data.items() if x.startswith('relatedPublication')]
         publication_values = [{'publicationCitation': {
-            'typeName': 'publicationCitation', 'multiple': 'false', 'typeClass': 'primitive', 'value': x}} for x in publication_values_temp]
+            'typeName': 'publicationCitation', 'multiple': False, 'typeClass': 'primitive', 'value': x}} for x in publication_values_temp]
         contactName_values_temp = [
             y for x, y in form_data.items() if x.startswith('datasetContactName')]
         contactName_values = [{
             "datasetContactName": {
                 "typeName": "datasetContactName",
-                "multiple": 'false',
+                "multiple": False,
                 "typeClass": "primitive",
                 "value": x
             }} for x in contactName_values_temp]
@@ -53,7 +54,7 @@ def index(req_path):
             y for x, y in form_data.items() if x.startswith('datasetContactEmail')]
         contactEmail_values = [{"datasetContactEmail": {
             "typeName": "datasetContactEmail",
-            "multiple": 'false',
+            "multiple": False,
             "typeClass": "primitive",
             "value": x
         }} for x in contactEmail_values_temp]
@@ -62,7 +63,7 @@ def index(req_path):
         contributor_values = [{
             "datasetContactName": {
                 "typeName": "datasetContactName",
-                "multiple": 'false',
+                "multiple": False,
                 "typeClass": "primitive",
                 "value": x
             }} for x in contributor_values_temp]
@@ -71,7 +72,7 @@ def index(req_path):
         software_values_temp = [
             y for x, y in form_data.items() if x.startswith('software')]
         software_values = [{'softwareName': {
-            'typeName': 'softwareName', 'multiple': 'false', 'typeClass': 'primitive', 'value': software}} for software in software_values_temp]
+            'typeName': 'softwareName', 'multiple': False, 'typeClass': 'primitive', 'value': software}} for software in software_values_temp]
         date_of_deposit_value = form_data.get('depositDate')
         date_start_value = form_data.get('dateRangeStart')
         date_end_value = form_data.get('dateRangeEnd')
@@ -80,46 +81,45 @@ def index(req_path):
         depositorName_value = 'test'
         licence_description_value = 'null'
         dv_metadata = {"datasetVersion": {"metadataBlocks": {"citation": {'displayName': "Citation Metadata",
-                                                                          "fields": [{'typeName': 'publication', 'value': publication_values, 'multiple': 'true', 'typeClass': 'compound'},
+                                                                          "fields": [{'typeName': 'publication', 'value': publication_values, 'multiple': True, 'typeClass': 'compound'},
                                                                                      {'typeName': 'author', 'typeClass': 'compound', 'value': author_values},
-                                                                                     {'typeName': 'keyword', 'typeClass': 'conmpound', 'multiple': 'true', 'value':  keyword_values,
+                                                                                     {'typeName': 'keyword', 'typeClass': 'compound', 'multiple': True, 'value':  keyword_values,
                                                                                       },
-                                                                                     {'typeName': 'subject', 'typeClass': 'true', 'value':  {'subject': {
-                                                                                         'typeName': 'subject', 'typeClass': 'primitive', 'value': subject_value}},
+                                                                                     {'typeName': 'subject', 'typeClass': 'controlledVocabulary', 'multiple': True, 'value': subject_value
                                                                                       },
-                                                                                     {'typeName': 'title', 'typeClass': 'primitive', 'value': title_value},
+                                                                                     {'typeName': 'title', 'multiple': False, 'typeClass': 'primitive', 'value': title_value},
                                                                                      {'typeName': 'dsDescription', 'typeClass': 'compound', 'value':  {'dsDescriptionValue': {
                                                                                          'typeName': 'dsDescriptionValue', 'typeClass': 'primitive', 'value': description_value}}},
                                                                                      {'typeName': 'contributor', 'typeClass': 'compound', 'value':  contributor_values},
                                                                                      {'typeName': 'datasetContact', 'value': [
                                                                                          *contactEmail_values, *contactName_values]},
                                                                                      {'typeName': 'language', 'typeClass': 'controlledVocabulary',
-                                                                                      'multiple': 'true', 'value': language_values},
-                                                                                     {'typeName': 'software', 'multiple': 'true',
+                                                                                      'multiple': True, 'value': language_values},
+                                                                                     {'typeName': 'software', 'multiple': True,
                                                                                       'typeClass': 'primitive', 'value': software_values},
                                                                                      {'typeName': 'dateOfDeposit', 'typeClass': 'primitive', 'value': date_of_deposit_value},
-                                                                                     {'typeName': 'timePeriodCovered', 'typeClass': 'compound', 'multiple': 'true', 'value': [{"timePeriodCoveredStart": {
+                                                                                     {'typeName': 'timePeriodCovered', 'typeClass': 'compound', 'multiple': True, 'value': [{"timePeriodCoveredStart": {
                                                                                          "typeName": "timePeriodCoveredStart",
-                                                                                         "multiple": 'false',
+                                                                                         "multiple": False,
                                                                                          "typeClass": "primitive",
                                                                                          "value": date_start_value
                                                                                      },
                                                                                          "timePeriodCoveredEnd": {
                                                                                          "typeName": "timePeriodCoveredEnd",
-                                                                                         "multiple": 'false',
+                                                                                         "multiple": False,
                                                                                          "typeClass": "primitive",
                                                                                          "value": date_end_value
                                                                                      }}]},
                                                                                      {
                                                                               "typeName": "kindOfData",
-                                                                              "multiple": 'true',
+                                                                              "multiple": True,
                                                                               "typeClass": "primitive",
                                                                               "value":
                                                                               data_type_values
                                                                           },
                                                                               {
                                                                               "typeName": "depositor",
-                                                                              "multiple": 'false',
+                                                                              "multiple": False,
                                                                               "typeClass": "primitive",
                                                                               "value": depositorName_value
                                                                           }]
@@ -131,10 +131,11 @@ def index(req_path):
                                           },
                        "licence": {"name": licence_value},
                        'value': licence_description_value}
-        # with open('./data/dv_metadata.json', 'w') as dv_metadata_file:
-        #     dump(dv_metadata, dv_metadata_file, indent=4)
-        dv_metadata = dumps(dv_metadata)
-        r = requests.post(server_url+'api/dataverses/UMCU/datasets',headers=api_token, data=dv_metadata)
+        with open('./data/dv_metadata.json', 'w') as dv_metadata_file:
+            dump(dv_metadata, dv_metadata_file, indent=4)
+        api_url = Template('$server_url/api/dataverses/$/datasets')
+        r = requests.post(api_url, headers={
+                          'X-Dataverse-key': api_token}, data=dumps(dv_metadata))
         return r.json()
         # redirect you to the dataset on dataverse
         # return redirect(server_url)
