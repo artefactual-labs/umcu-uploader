a142a45d7db10104dae72dc27e8d2dfb6e815866
Mike Cantelon <mcantelon@gmail.com>
2022-11-16 14:37:21 -0800

    wip

--- uploader/Transfer/views.py
+++ uploader/Transfer/views.py
@@ -3,6 +3,7 @@
 import os
 from flask import Blueprint, render_template, request, session, flash, redirect, url_for
 from flask_api import status
+from werkzeug.utils import secure_filename
 
 from uploader.Transfer import helpers
 
@@ -34,3 +35,28 @@ def update():
             return redirect(url_for('transfer.index'))
 
     return "Unsuitable form data submitted", status.HTTP_400_BAD_REQUEST
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+#ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@transfer.route('/upload', methods=['POST'])
+def upload():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(url_for('transfer.index'))
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(url_for('transfer.index'))
+    #if file and allowed_file(file.filename):
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('download_file', name=filename))
