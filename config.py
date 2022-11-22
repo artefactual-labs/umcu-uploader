cccb3df96bfa112641b95b150b9e4d1420568e40
Mike Cantelon <mcantelon@gmail.com>
2022-11-22 01:56:18 -0800

    Add department-based transfer source dir config
    * Add application configuration file support
    
    * Add ability to use department configuration to select a transfer
      source directory

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 import os
+import yaml
+
+
+# Attempt to load application configuration
+app_config_filepath = "/etc/umcu-uploader.yaml"
+
+if os.path.isfile(app_config_filepath):
+    with open(app_config_filepath, "r") as stream:
+        app_config = yaml.safe_load(stream)
+
 
 class Config:
     # Be sure to set a secure secret key for production.
@@ -6,7 +16,15 @@ class Config:
 
     DATA_DIRECTORY = os.getenv("DATA_DIRECTORY")
     TRANSFER_SOURCE_DIRECTORY = os.getenv("TRANSFER_SOURCE_DIRECTORY")
+    DEPARTMENTS = {}
+
     DEBUG = os.getenv("DEBUG") == "True"
     TESTING = False
+
+
+# Set departments configuration, if available
+if "app_config" in locals() and "departments" in app_config:
+    Config.DEPARTMENTS = app_config['departments']
+
 
 CONFIGS = {"default": Config}
--- uploader/Upload/views.py
+++ uploader/Upload/views.py
@@ -18,34 +18,57 @@ from uploader.Transfer import helpers
 upload = Blueprint("upload", __name__, template_folder="templates")
 
 
-@upload.route("/", methods=["GET"])
+@upload.route("/", methods=["GET", "POST"])
 def index():
     transfer_dir = helpers.get_transfer_directory()
     transfer_file_count, transfer_total_file_size = helpers.directory_count_and_size(
         transfer_dir
     )
 
+    # Assemble department options
+    department_options = []
+    for department_id in current_app.config['DEPARTMENTS'].keys():
+        department = current_app.config['DEPARTMENTS'][department_id]
+        department_options.append({"id": department_id, "name": department["name"]})
+
+    transfer_source_dir = current_app.config["TRANSFER_SOURCE_DIRECTORY"]
+
+    # Handle form submission
+    if request.method == 'POST':
+        transfer_name = request.form["transfer_name"]
+        department_id = request.form["department_id"]
+
+        # Make sure necessary values have been provided
+        validation_failed = False
+
+        if transfer_name == "":
+            flash("Please enter a transfer name.", "danger")
+            validation_failed = True
+
+        if not transfer_source_dir:
+            if department_id == "":
+                flash("Please select a department.", "danger")
+                validation_failed = True
+
+        if not validation_failed:
+            # Perform copy asynchronously
+            if not transfer_source_dir:
+                transfer_source_dir = current_app.config['DEPARTMENTS'][department_id]['transfer_source_directory']
+
+            destination_dir = os.path.join(transfer_source_dir, transfer_name)
+
+            u = job.CreateTransferJob()
+            u.params({"source": transfer_dir, "destination": destination_dir})
+            u.start()
+
+            flash("Copy started.", "primary")
+
     return render_template(
         "upload.html",
         transfer_directory=transfer_dir,
         transfer_file_count=transfer_file_count,
         transfer_total_file_size=transfer_total_file_size,
+        department_options=department_options,
+        transfer_source_dir=transfer_source_dir,
+        request=request
     )
-
-
-@upload.route("/", methods=["POST"])
-def send():
-    transfer_dir = helpers.get_transfer_directory()
-    transfer_source_dir = current_app.config["TRANSFER_SOURCE_DIRECTORY"]
-
-    transfer_name = request.form["transfer_name"]
-
-    # Perform copy asynchronously
-    destination_dir = os.path.join(transfer_source_dir, transfer_name)
-
-    u = job.CreateTransferJob()
-    u.params({"source": transfer_dir, "destination": destination_dir})
-    u.start()
-
-    flash("Copy started.", "primary")
-    return redirect(url_for("upload.index"))
