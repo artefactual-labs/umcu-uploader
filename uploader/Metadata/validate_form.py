3db0c1401726112648251dc36d9213756ccfd57e
Diogenesoftoronto <diogenesoft@protonmail.com>
2022-12-02 15:00:32 -0500

    feat: :sparkles: add validation
    validate the user input from the form and stop the server from taking arbitrary inputs from the client!

---
+++ uploader/Metadata/validate_form.py
@@ -0,0 +1,67 @@
+import re
+
+def validate_metadata_form(form: dict) -> bool:
+    """
+    Validate metadata form
+    """
+    # Check for required fields
+    required_fields = [
+        "title",
+        "author",
+        "dsDescription",
+        "licenseType",
+        "keyword",
+        "contributor",
+        "software",
+        "dataType",
+        "relatedPublication",
+        "datasetContactName",
+        "datasetContactEmail",
+        "depositDate",
+        "researchType",
+        "dateRangeStart",
+        "dateRangeEnd",
+        "researchEndDate",
+    ]
+    # Check for required fields
+    
+    for field in required_fields:
+        if field not in form:
+            return False
+    # check that datasetContactEmail is a valid email
+    if not validate_email(form["datasetContactEmail"]):
+        return False
+    return True
+
+def validate_email(email: any)-> bool:
+    """
+    Validate email
+    """
+    try: 
+        # Check if email is a string
+        if not isinstance(email, str):
+            raise EmailNotStringError
+        # Match email with a regex
+        if re.fullmatch(r'') is None:
+            raise EmailSyntaxError
+    except EmailNotStringError | EmailSyntaxError:
+        return False
+    return True
+
+class EmailInvalidError(Exception):
+    """
+    Exception raised for email invalid errors
+    """
+    pass
+
+class EmailNotStringError(EmailInvalidError):
+    """
+    Exception raised for email not string errors
+    """
+    pass
+
+class EmailSyntaxError(EmailInvalidError):
+    """
+    Exception raised for email syntax errors
+    """
+    pass
