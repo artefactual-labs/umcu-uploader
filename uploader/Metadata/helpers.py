4045ae4df0928f6489bc58dce939a2e1a618c9eb
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-12-12 18:18:34 -0500

    fix: :ambulance: string conversion
    python cannot both add and convert strings for some reason :(
    this fixes that

--- uploader/Metadata/helpers.py
+++ uploader/Metadata/helpers.py
@@ -2,24 +2,25 @@ import csv
 
 
 def add_year(date: str, year: int) -> str:
-        """
-        Adds a date to the date of deposit field
-        """
-        year_from_date = int(date[:4])
-        rest_of_date = date[4:]
-        sum_year = str(year_from_date + year)
-        full_date = sum_year + rest_of_date
-        return full_date
+    """
+    Adds a date to the date of deposit field
+    """
+    year_from_date = int(date[:4])
+    rest_of_date = date[4:]
+    sum_year = year_from_date + year
+    string_sum_year = str(sum_year)
+    full_date = string_sum_year + rest_of_date
+    return full_date
 
 
 def get_retention(date: str, researchType: str) -> str:
     retention_value = None
-    if researchType == 'Basic':
-            retention_num = 15
+    if researchType == "Basic":
+        retention_num = 15
     elif researchType == "Medication":
-            retention_num = 25
+        retention_num = 25
     elif researchType == "Therapeutic":
-            retention_num = 30
+        retention_num = 30
     retention_value = add_year(date, retention_num)
     return retention_value
 
@@ -28,14 +29,15 @@ def get_raw_data(data: dict, name: str) -> list:
     list = [y for x, y in data.items() if x.startswith(name)]
     return list
 
-def get_division_acronym(divisions_file_path: str, division: str) -> str: 
-        # loop through the divisions csv and match the division name with the acronym
-        # return the acronyms
-        acronym = None
-        with open(divisions_file_path, 'r') as divisions_file:
-                divisions_csv_reader = csv.reader(divisions_file, delimiter=';')
-                for row in divisions_csv_reader:
-                        if division ==  row[0]:
-                                acronym = row[1]
-                                return acronym
-                return 'No acroynm found'
+
+def get_division_acronym(divisions_file_path: str, division: str) -> str:
+    # loop through the divisions csv and match the division name with the acronym
+    # return the acronyms
+    acronym = None
+    with open(divisions_file_path, "r") as divisions_file:
+        divisions_csv_reader = csv.reader(divisions_file, delimiter=";")
+        for row in divisions_csv_reader:
+            if division == row[0]:
+                acronym = row[1]
+                return acronym
+        return "No acroynm found"
