cac6ac77a11c6a20fb3254f8856be39ab2e1e16d
Mike Cantelon <mcantelon@gmail.com>
2022-12-14 15:13:56 -0800

    Directory upload UI improvement
    On transfer upload page change "Choose file" to "Choose directory".
    Hide directory picker and submit button when uploading and show
    "spinner" animation.

---
+++ uploader/static/upload.js
@@ -0,0 +1,24 @@
+const $chooserProxy = document.getElementById("chooserProxy");
+const $chooser = document.getElementById("chooser");
+const $chooserSubmit = document.getElementById("chooserSubmit");
+const $chooserSpinner = document.getElementById("chooserSpinner");
+const $chooserFeedback = document.getElementById("chooserFeedback");
+
+// Show directory picker when proxy is clicked
+$chooserProxy.addEventListener("click", function() {
+  $chooser.click();
+});
+
+// Show number of file after directory is selected
+$chooser.addEventListener("change", function() {
+  const message = ($chooser.files.length) + " files selected";
+  $chooserFeedback.textContent = message;
+});
+
+// Hide directory picker, etc., and show spinner when starting upload
+$chooserSubmit.addEventListener("click", function() {
+  $chooserProxy.style.display = "none";
+  $chooserSubmit.style.display = "none";
+  $chooserFeedback.style.display = "none";
+  $chooserSpinner.classList.remove("invisible");
+});
